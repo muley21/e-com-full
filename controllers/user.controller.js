da9ef8560d0a26e0e76aeaa3c09afa3b1f53bedc
Neha Muley <nehamuley955@gmail.com>
2024-06-29 14:49:56 +0530

    sorting update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -10,7 +10,10 @@ const asyncHandler = require("express-async-handler")
 const Order = require("../models/Order")
 
 exports.userGetAllOrders = asyncHandler(async (req, res) => {
-    const result = await Order.find({ user: req.params.id }).populate("products.product")
+    const result = await Order.
+        find({ user: req.params.id })
+        .sort({ createdAt: 1 })
+        .populate("products.product")
     res.json({ message: " User Fetch Success", result })
 })
 
